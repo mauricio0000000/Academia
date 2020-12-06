c59190dd252c8ce363f5e910a31b736f203090e5
mauricio0000000 <44041866+mauricio0000000@users.noreply.github.com>
2020-12-05 21:48:35 -0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Academia.Startup))]
+namespace Academia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
